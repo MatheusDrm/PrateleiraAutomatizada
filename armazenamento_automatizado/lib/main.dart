43fc693a435f104bf8f44546a60dc78483afa456
Lucas Gambier Costa Mendes <lucasgcm@usp.br>
2023-07-06 08:00:25 -0300

    fudeu d+ matheus

--- armazenamento_automatizado/lib/main.dart
+++ armazenamento_automatizado/lib/main.dart
@@ -1,69 +1,246 @@
 import 'package:flutter/material.dart';
-
-import 'home.dart';
-import 'Guardar.dart';
-import 'Retirar.dart';
+import 'package:flutter/services.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 
+void main(){
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  static const String _title = 'Flutter Code Sample';
+class MyApp extends StatefulWidget{
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      debugShowCheckedModeBanner: false,
-      home: RootPage(),
-    );
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State{
+
+  late IO.Socket socket;
+
+  @override
+  void initState() {
+    initSocket();
+    super.initState();
   }
-}
-
-class RootPage extends StatefulWidget {
-  const RootPage({Key? key}) : super(key: key);
-
-  @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  int currentPage = 0;
-  List<Widget> pages = const [
-    Guardar(),
-    Home(),
-    Retirar()
-  ];
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
+  initSocket() {
+    socket = IO.io('http://192.168.0.8:80', <String, dynamic>{
+      'autoConnect': false,
+      'transports': ['websocket'],
     });
+    socket.connect();
+    socket.onConnect((_) {
+      print('Connection established');
+    });
+    socket.onDisconnect((_) => print('Connection Disconnection'));
+    socket.onConnectError((err) => print(err));
+    socket.onError((err) => print(err));
   }
 
+  retirar(int comp){
+    socket.emit("retirar", comp);
+  }
+
+  salvar(String cliente, String objeto, String compartimento){
+    socket.emit('salvar', [cliente, objeto, compartimento]);
+  }
+
+
+ GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  String? _selectedProduct;
+  late var cliente;
+  late var objeto;
+  late var compartimento;
+  List<String> _products = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+  ];
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white30,
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: const Text('Armazém automatizado',),
-      ),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.deepPurple,
-        items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.all_inbox_rounded,), label: 'Guardar'),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.assignment_sharp), label: 'Retirar')
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'armazenamento_automatizado',
+      home:Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.purple[900]!,
+                Colors.purple[800]!,
+                Colors.purple[400]!
+              ],
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height:  40,),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Armazem automático", style: TextStyle(color: Colors.white, fontSize: 35)),
+                      SizedBox(height: 10,),
+                      Text("Escolha uma operação:", style: TextStyle(color: Colors.white, fontSize: 18))
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black54),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                    height: 300,
+                    width: 400,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      Text('Retirar', style: TextStyle(fontSize: 20, color: Colors.white),),
+                      SizedBox(height: 15,),
+                      SizedBox(
+                        width: 370,
+                        height: 240,
+                        child: GridView.count(
+                          crossAxisCount: 3,
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          children: [
+                            ElevatedButton(onPressed: (){retirar(1);}, child: Text('1'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                            ElevatedButton(onPressed: (){retirar(2);}, child: Text('2'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                            ElevatedButton(onPressed: (){retirar(3);}, child: Text('3'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                            ElevatedButton(onPressed: (){retirar(4);}, child: Text('4'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                            ElevatedButton(onPressed: (){retirar(5);}, child: Text('5'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                            ElevatedButton(onPressed: (){retirar(6);}, child: Text('6'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!),),),
+
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  ),
+                SizedBox(height: 15,),
+              Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black54),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  height: 300,
+                  width: 400,
+                  child: Column(
+                      children: [
+                        SizedBox(height: 20),
+                        Text('Guardar', style: TextStyle(fontSize: 20, color: Colors.white),),
+                        SizedBox(height: 15,),
+                        SizedBox(
+                          width: 370,
+                          height: 240,
+                          child: Form(
+                            key: this._formKey,
+                            child: Column(
+                              children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                                child: TextFormField(
+                                style: TextStyle(color: Colors.white),
+                                decoration: InputDecoration(
+                                  labelText: "Nome do cliente",
+                                  labelStyle: TextStyle(color: Colors.white),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return "Por favor, insira seu nome";
+                                  }
+                                  return null;
+                                },
+                                  onSaved: (value){
+                                    this.cliente = value;
+                                  },
+                            ),
+                              ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                                  child: TextFormField(
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      labelText: "Nome do objeto",
+                                      labelStyle: TextStyle(color: Colors.white),
+                                      enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.white),
+                                      ),
+                                      focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.white),
+                                      ),
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return "Por favor, insira o nome do objeto";
+                                      }
+                                      return null;
+                                    },
+                                    onSaved: (value){
+                                      this.objeto = value;
+                                    },
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                                  child: DropdownButtonFormField<String>(
+                                    value: _selectedProduct,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      labelText: "Compartimento",
+                                      labelStyle: TextStyle(color: Colors.white),
+                                      enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.white),
+                                      ),
+                                      focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.white),
+                                      ),
+                                    ),
+                                    items: _products.map((String product) {
+                                      return DropdownMenuItem<String>(
+                                        value: product,
+                                        child: Text(product, style: TextStyle(color: Colors.white)),
+                                      );
+                                    }).toList(),
+                                    onChanged: (String? value) {
+                                      setState(() {
+                                        _selectedProduct = value;
+                                      });
+                                    },
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return "Por favor, selecione um produto";
+                                      }
+                                      return null;
+                                    },
+                                    onSaved: (value){this.objeto = value;},
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                ElevatedButton(onPressed: (){print(this.cliente);salvar(this.cliente, this.objeto, this.compartimento);}, child: Text('Enviar'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[900]!)),),
+                              ],
+                            ),
+                          ),
+                  )]
+                  )
+              )
+              ],
+            ),
+          ),
+        )
+        ),
+      );
   }
 }
